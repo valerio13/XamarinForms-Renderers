01870c16b7512f485715c69d430ce1871345c4ce
Valerio <valerio.colantonio@gmail.com>
2021-12-21 11:44:30 +0100

    Added Forms project

---
+++ Pruebas_componentes/Controls/BasicControls/BasicSlider.cs
@@ -0,0 +1,8 @@
+﻿using Xamarin.Forms;
+
+namespace OneyApp.Controls.BasicControls
+{
+    public class BasicSlider : Slider
+    {
+    }
+}
---
+++ Pruebas_componentes/Controls/OneyControls/OneySlider.cs
@@ -0,0 +1,11 @@
+﻿using OneyApp.Controls.BasicControls;
+
+namespace OneyApp.Controls.OneyControls
+{
+    public class OneySlider: BasicSlider
+    {
+        public OneySlider()
+        {
+        }
+    }
+}
